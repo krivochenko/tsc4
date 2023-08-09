af00c7e454c8c6e3061ee74ce009a5a7745bdbd8
Sergei Krivochenko <admin@krivochenko.ru>
2023-08-09 20:41:33 -0300

    Task 2

--- tests/Task2.spec.ts
+++ tests/Task2.spec.ts
@@ -5,34 +5,40 @@ import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
 describe('Task2', () => {
-    let code: Cell;
+  let code: Cell;
 
-    beforeAll(async () => {
-        code = await compile('Task2');
+  beforeAll(async () => {
+    code = await compile('Task2');
+  });
+
+  let blockchain: Blockchain;
+  let task2: SandboxContract<Task2>;
+
+  beforeEach(async () => {
+    blockchain = await Blockchain.create();
+
+    task2 = blockchain.openContract(Task2.createFromConfig({}, code));
+
+    const deployer = await blockchain.treasury('deployer');
+
+    const deployResult = await task2.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+    expect(deployResult.transactions).toHaveTransaction({
+      from: deployer.address,
+      to: task2.address,
+      deploy: true,
+      success: true,
     });
+  });
 
-    let blockchain: Blockchain;
-    let task2: SandboxContract<Task2>;
+  it('should get size of matrix', async () => {
+    const sizeResult = await task2.getMatrixSize([[1n, 2n, 3n, 4n], [5n, 6n, 7n, 8n]]);
+    expect(sizeResult).toEqual([2n, 4n]);
+  });
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
 
-        task2 = blockchain.openContract(Task2.createFromConfig({}, code));
-
-        const deployer = await blockchain.treasury('deployer');
-
-        const deployResult = await task2.sendDeploy(deployer.getSender(), toNano('0.05'));
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task2.address,
-            deploy: true,
-            success: true,
-        });
-    });
-
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and task2 are ready to use
-    });
+  it('should multiply matrix', async () => {
+    const sizeResult = await task2.getMatrixMultiplier([[1n, 2n, 3n, 4n], [5n, 6n, 7n, 8n]], [[1n, 2n], [3n, 4n], [5n, 6n], [7n, 8n]]);
+    expect(sizeResult).toEqual([[50n, 60n], [114n, 140n]]);
+  });
 });
--- wrappers/Task2.ts
+++ wrappers/Task2.ts
@@ -1,29 +1,86 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
+import {
+  Address,
+  beginCell,
+  Cell,
+  Contract,
+  contractAddress,
+  ContractProvider,
+  Sender,
+  SendMode, Tuple,
+  TupleItem, TupleReader
+} from 'ton-core';
 
 export type Task2Config = {};
 
 export function task2ConfigToCell(config: Task2Config): Cell {
-    return beginCell().endCell();
+  return beginCell().endCell();
 }
 
 export class Task2 implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+  constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {
+  }
 
-    static createFromAddress(address: Address) {
-        return new Task2(address);
-    }
+  static createFromAddress(address: Address) {
+    return new Task2(address);
+  }
 
-    static createFromConfig(config: Task2Config, code: Cell, workchain = 0) {
-        const data = task2ConfigToCell(config);
-        const init = { code, data };
-        return new Task2(contractAddress(workchain, init), init);
-    }
+  static createFromConfig(config: Task2Config, code: Cell, workchain = 0) {
+    const data = task2ConfigToCell(config);
+    const init = { code, data };
+    return new Task2(contractAddress(workchain, init), init);
+  }
 
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-        await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
-        });
+  async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+    await provider.internal(via, {
+      value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell().endCell(),
+    });
+  }
+
+  async getMatrixSize(provider: ContractProvider, matrix: bigint[][]): Promise<[bigint, bigint]> {
+    const matrixTuple: TupleItem[] = matrix.map((row) => ({
+      type: 'tuple',
+      items: row.map((value) => ({ type: 'int', value: value }))
+    }));
+
+    const result = await provider.get('get_matrix_size', [{
+      type: 'tuple',
+      items: matrixTuple,
+    }]);
+    const tuple = result.stack.readTuple();
+    return [tuple.readBigNumber(), tuple.readBigNumber()];
+  }
+
+  async getMatrixMultiplier(provider: ContractProvider, matrixA: bigint[][], matrixB: bigint[][]): Promise<any> {
+    const matrixATuple: TupleItem[] = matrixA.map((row) => ({
+      type: 'tuple',
+      items: row.map((value) => ({ type: 'int', value })),
+    }));
+    const matrixBTuple: TupleItem[] = matrixB.map((row) => ({
+      type: 'tuple',
+      items: row.map((value) => ({ type: 'int', value })),
+    }));
+
+    const result = await provider.get('matrix_multiplier', [{
+      type: 'tuple',
+      items: matrixATuple,
+    }, {
+      type: 'tuple',
+      items: matrixBTuple,
+    }]);
+
+    const matrix = [];
+
+    const matrixTuple: TupleReader = result.stack.readTuple();
+    while (!!matrixTuple.remaining) {
+      const rowTuple = matrixTuple.readTuple();
+      const row = [];
+      while (!!rowTuple.remaining) {
+        row.push(rowTuple.readBigNumber());
+      }
+      matrix.push(row);
     }
+    return matrix;
+  }
 }
