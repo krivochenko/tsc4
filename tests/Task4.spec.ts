ef2ca4df7489200bab275ddf792be7b9f26a262b
Sergei Krivochenko <admin@krivochenko.ru>
2023-08-13 04:56:23 -0300

    Task 4

--- tests/Task4.spec.ts
+++ tests/Task4.spec.ts
@@ -1,38 +1,39 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { beginCell, Cell, toNano } from 'ton-core';
 import { Task4 } from '../wrappers/Task4';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
 
 describe('Task4', () => {
-    let code: Cell;
+  let code: Cell;
 
-    beforeAll(async () => {
-        code = await compile('Task4');
+  beforeAll(async () => {
+    code = await compile('Task4');
+  });
+
+  let blockchain: Blockchain;
+  let task4: SandboxContract<Task4>;
+
+  beforeEach(async () => {
+    blockchain = await Blockchain.create();
+
+    task4 = blockchain.openContract(Task4.createFromConfig({}, code));
+
+    const deployer = await blockchain.treasury('deployer');
+
+    const deployResult = await task4.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+    expect(deployResult.transactions).toHaveTransaction({
+      from: deployer.address,
+      to: task4.address,
+      deploy: true,
+      success: true,
     });
+  });
 
-    let blockchain: Blockchain;
-    let task4: SandboxContract<Task4>;
+  it('should encrypt', async () => {
+    const result = await task4.getEncrypt(1n, beginCell().storeUint(0, 32).storeStringTail('aa').endCell());
 
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-
-        task4 = blockchain.openContract(Task4.createFromConfig({}, code));
-
-        const deployer = await blockchain.treasury('deployer');
-
-        const deployResult = await task4.sendDeploy(deployer.getSender(), toNano('0.05'));
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: task4.address,
-            deploy: true,
-            success: true,
-        });
-    });
-
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and task4 are ready to use
-    });
+    expect(result).toEqualCell(beginCell().storeUint(0, 32).storeStringTail('bb').endCell());
+  });
 });
